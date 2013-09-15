81d2c2e622804c506e03600763619ecd678f9380
Ivan Nemytchenko <nemytchenko@gmail.com>
2013-09-15 23:54:16 +0700

    fixed first line indentation problem

--- formatter.rb
+++ formatter.rb
@@ -7,18 +7,23 @@ space_sympol = "  "
 line = ""
 
 end_of_line = false
+should_add_counter = false
 previous_character = nil
 
 input.split("").each do |character|
   if end_of_line
     puts "#{space_sympol*counter}#{line}#{previous_character}"
+    counter = counter + 1 if should_add_counter
+    should_add_counter = false
     line = ""
   end
 
   end_of_line = false
   
+
   if character == "{"
-    counter = counter + 1
+    should_add_counter = true
+    # counter = counter + 1
   elsif character == "}"
     counter = counter - 1
   elsif character == ";"
