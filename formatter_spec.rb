57b79066dd32f5f765352985992d8f25f26c249c
Ivan Nemytchenko <nemytchenko@gmail.com>
2013-09-15 23:51:38 +0700

    spec and plan added

---
+++ formatter_spec.rb
@@ -0,0 +1,19 @@
+require "rubygems"
+require "rspec"
+
+describe "formatter" do
+  it "should restore broken formatting" do
+
+  end
+
+  it "should not break correct formatting" do
+
+  end
+end
+
+
+# Plan:
+# 1. create example file with good formatting
+# 2. make a copy and break formatting
+# 3. create assertion that formatter converts both of them to first version
+# 
