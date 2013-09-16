bdf3a345505e29155d60e0f0807b44eda2749391
Ivan Nemytchenko <nemytchenko@gmail.com>
2013-09-16 12:26:28 +0700

    one more spec

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe "formatter" do
-  it "should restore broken formatting" do
+  it "should restore oneliner" do
     `ruby formatter.rb < ./code_samples/oneliner.code`.should eql `cat ./code_samples/formatted.code`
   end
 
   it "should not break correct formatting" do
     `ruby formatter.rb < ./code_samples/formatted.code`.should eql `cat ./code_samples/formatted.code`
+  end
+
+  it "should restore broken formatting" do
+    `ruby formatter.rb < ./code_samples/broken.code`.should eql `cat ./code_samples/formatted.code`
   end
 end
 
