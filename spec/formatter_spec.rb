449c3e0ace98a4993d39b8727a75952477504f68
Ivan Nemytchenko <nemytchenko@gmail.com>
2013-09-16 12:38:46 +0700

    removed comments

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -13,10 +13,3 @@ describe "formatter" do
     `ruby formatter.rb < ./code_samples/broken.code`.should eql `cat ./code_samples/formatted.code`
   end
 end
-
-
-# Plan:
-# 1. create example file with good formatting
-# 2. make a copy and break formatting
-# 3. create assertion that formatter converts both of them to first version
-# 
