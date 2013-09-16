3002c2608e1604d5e88ee41dd1b9201d7881a04d
Ivan Nemytchenko <nemytchenko@gmail.com>
2013-09-16 16:31:41 +0700

    first steps of OOP

--- formatter.rb
+++ formatter.rb
@@ -1,40 +1,91 @@
 #!/usr/bin/ruby
  
-input = $stdin.read
-
-counter = 0
-space_sympol = "  "
-line = ""
-
-end_of_line = false
-should_add_counter = false
-previous_character = nil
-
-input.split("").each do |character|
-  if end_of_line
-    puts "#{space_sympol*counter}#{line}#{previous_character}"
-    counter = counter + 1 if should_add_counter
-    should_add_counter = false
-    line = ""
+class Reader
+  attr_accessor :path, :file
+  def initialize(path)
+    @path = path
+    @file = File.open(path,'r')
   end
 
-  end_of_line = false
-  
-
-  if character == "{"
-    should_add_counter = true
-    # counter = counter + 1
-  elsif character == "}"
-    counter = counter - 1
-  elsif character == ";"
-    end_of_line = true
+  def getc
+    character_code = @file.getc
+    if character_code
+      character_code.chr 
+    else
+      false
+    end
   end
-
-  unless end_of_line
-    line << character
-  end 
-  
-  # puts output
-  previous_character = character
 end
-puts line
+
+class Cfg
+  attr_accessor :spacer, :block_start, :block_end, :new_line_after
+  def initialize(spacer, block_start, block_end, new_line_after)
+    @spacer = spacer
+    @block_start = block_start
+    @block_end = block_end
+    @new_line_after = new_line_after
+    
+  end
+end
+
+class Formatter
+  attr_accessor :reader, :config
+
+  def initialize(reader)
+    @config = Cfg.new("  ", "{", "}", ";")
+    @reader = reader
+  end
+
+  def proceed
+
+  end
+end
+
+
+reader = Reader.new('./code_samples/oneliner.code')
+formatter = Formatter.new
+
+while true do
+  character = reader.getc
+  exit unless character
+
+  print character
+end
+
+# input = $stdin.read
+
+# counter = 0
+# space_sympol = "  "
+# line = ""
+
+# end_of_line = false
+# should_add_counter = false
+# previous_character = nil
+
+# input.split("").each do |character|
+#   if end_of_line
+#     puts "#{space_sympol*counter}#{line}#{previous_character}"
+#     counter = counter + 1 if should_add_counter
+#     should_add_counter = false
+#     line = ""
+#   end
+
+#   end_of_line = false  
+
+#   if character == "{"
+#     should_add_counter = true
+#     # counter = counter + 1
+#   elsif character == "}"
+#     counter = counter - 1
+#   elsif character == ";"
+#     end_of_line = true
+#   end
+
+#   unless end_of_line
+#     line << character
+#   end 
+  
+#   # puts output
+#   previous_character = character
+# end
+# puts line
